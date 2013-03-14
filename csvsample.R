c5a1d6b9e79f87af5d7a44b770e6a511e1108367
erkwang <erikent@yahoo.com>
2013-03-14 15:31:12 -0700

    code to sample within one csv file on each node of the cluster

---
+++ csvsample.R
@@ -0,0 +1,49 @@
+#STA 242 Homework 5
+#Yichuan Wang
+
+
+#obtaining one fifth of the total observations in each year without replacement
+#implementing the explicit parallelization mechanism
+#the exact script will be running on each node but using node ID to identify
+#which csv file should be read
+
+#load csv file paths
+load("~/hw4/csvpath.rda")
+
+#obtain the specific csv file to be operated on
+csvid = Sys.getenv('SGE_TASK_ID')
+csvfile = csvpath[csvid]
+
+#read all lines of the csv file, omitting first line which is the column names
+csvcon = file(csvfile, open = "rt" )
+csvlines = readLines(csvfile)[-1]
+
+#sample the indeces of lines to be selected
+#about one fifth of total observations will be selected
+sampleind = sample(1:length(csvlines), size = floor(length(csvlines)/5))
+
+#grab the sampled lines
+csvsample = csvlines[sampleind]
+
+#close connection
+close(csvcon)
+
+#open connection to write the lines to one file in an appending manner
+writecon = file("~/hw4/airlinesample", open = "at")
+writeLines(text = csvsample, con = writecon)
+close(writecon)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
